03e8ab4939a6794039fb4e5007697e3b6697c6b9
Brett Heinkel <bheinks@gmail.com>
2023-12-03 22:03:57 -0600

    Use 2 spaces

--- 2023/day1/day1.rb
+++ 2023/day1/day1.rb
@@ -1,22 +1,22 @@
 input = File.read(File.join(File.dirname(__FILE__), "input"))
 
 def part_1(input)
-    input.each_line.map do |line|
-        digits = line.scan(/\d+/).join
-        [digits[0], digits[-1]].join.to_i
-    end.sum
+  input.each_line.map do |line|
+    digits = line.scan(/\d+/).join
+    [digits[0], digits[-1]].join.to_i
+  end.sum
 end
 
 def part_2(input)
-    digit_map = %w(one two three four five six seven eight nine).map.with_index{|d, i| [d, i+1]}.to_h
-    digit_pattern = /(?=(\d|#{digit_map.keys.join('|')}))/
+  digit_map = %w(one two three four five six seven eight nine).map.with_index{|d, i| [d, i+1]}.to_h
+  digit_pattern = /(?=(\d|#{digit_map.keys.join('|')}))/
 
-    input.each_line.map do |line|
-        digits = line.scan(digit_pattern).flatten.map do |digit|
-            digit_map.fetch(digit, digit.to_i)
-        end
-        [digits[0], digits[-1]].join.to_i
-    end.sum
+  input.each_line.map do |line|
+    digits = line.scan(digit_pattern).flatten.map do |digit|
+      digit_map.fetch(digit, digit.to_i)
+    end
+    [digits[0], digits[-1]].join.to_i
+  end.sum
 end
 
 p part_1 input
