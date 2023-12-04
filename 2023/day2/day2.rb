aa4d498f5f51f9465ce2e99ca7dc16cd7cc46999
Brett Heinkel <bheinks@gmail.com>
2023-12-03 22:00:55 -0600

    Simplify pattern match

--- 2023/day2/day2.rb
+++ 2023/day2/day2.rb
@@ -1,14 +1,14 @@
 MAX_RED_CUBES = 12
 MAX_GREEN_CUBES = 13
 MAX_BLUE_CUBES = 14
-GAME_PATTERN = /(\d+ (?:red|green|blue))/
+GAME_PATTERN = /\d+ (?:red|green|blue)/
 
 input = File.read(File.join(File.dirname(__FILE__), "input"))
 
 def game_possible?(line)
     game_id, games = line.split(": ")
 
-    games.scan(GAME_PATTERN).flatten.each do |game|
+    games.scan(GAME_PATTERN).each do |game|
         count, color = game.split
         count = count.to_i
         if (color == 'red' && count > MAX_RED_CUBES) ||
@@ -31,7 +31,7 @@ def calculate_cube_power(line)
     games = line.split(": ")[1]
     red_max, green_max, blue_max = 0, 0, 0
 
-    games.scan(GAME_PATTERN).flatten.each do |game|
+    games.scan(GAME_PATTERN).each do |game|
         count, color = game.split
         count = count.to_i
         if color == 'red' && count > red_max
