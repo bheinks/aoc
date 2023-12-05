199da13424ae0a53bf1ce46e97c64c0b7f154a4b
Brett Heinkel <brett@heinkel.dev>
2023-12-05 01:23:37 -0600

    Submit day 3 part 2

--- 2023/day3/day3.rb
+++ 2023/day3/day3.rb
@@ -4,19 +4,25 @@ def is_symbol?(char)
   "*%$@&+/#=-".include? char
 end
 
-def borders_symbol?(input_arr, row_length, part_num, start_row, start_col)
+def neighbors_symbol?(input_arr, row_length, part_num, start_row, start_col, gears)
+  neighbor_found = false
+
   # Left
   if start_col > 0
-    if is_symbol? input_arr[start_row][start_col-1]
-      return true
+    char = input_arr[start_row][start_col-1]
+    if is_symbol? char
+      neighbor_found = true
+      gears[[start_row, start_col-1]] += [part_num.to_i] if char == "*"
     end
   end
 
   # Right
   col = start_col + part_num.length
   if col < row_length
-    if is_symbol? input_arr[start_row][col]
-      return true
+    char = input_arr[start_row][col]
+    if is_symbol? char
+      neighbor_found = true
+      gears[[start_row, col]] += [part_num.to_i] if char == "*"
     end
   end
 
@@ -24,8 +30,10 @@ def borders_symbol?(input_arr, row_length, part_num, start_row, start_col)
   if start_row > 0
     (start_col-1..start_col+part_num.length).each do |col|
       if col >= 0 && col < row_length
-        if is_symbol? input_arr[start_row-1][col]
-          return true
+        char = input_arr[start_row-1][col]
+        if is_symbol? char
+          neighbor_found = true
+          gears[[start_row-1, col]] += [part_num.to_i] if char == "*"
         end
       end
     end
@@ -35,30 +43,35 @@ def borders_symbol?(input_arr, row_length, part_num, start_row, start_col)
   if start_row < input_arr.length-1
     (start_col-1..start_col+part_num.length).each do |col|
       if col >= 0 && col < row_length
-        if is_symbol? input_arr[start_row+1][col]
-          return true
+        char = input_arr[start_row+1][col]
+        if is_symbol? char
+          neighbor_found = true
+          gears[[start_row+1, col]] += [part_num.to_i] if char == "*"
         end
       end
     end
   end
 
-  return false
+  return neighbor_found
 end
 
-def part_1(input)
+def solve(input)
   input_arr = input.split.map{|l| l.chars}
   row_length = input_arr[0].length
+  sum = 0
+  gears = Hash.new []
 
   input.delete("\n").enum_for(:scan, /\d+/).map do |part_num|
     position = Regexp.last_match.begin(0)
     row, col = position.divmod(row_length)
 
-    if borders_symbol? input_arr, row_length, part_num, row, col
-      part_num.to_i
-    else
-      0
+    if neighbors_symbol? input_arr, row_length, part_num, row, col, gears
+      sum += part_num.to_i
     end
-  end.sum
+  end
+
+  p sum
+  p gears.filter_map { |k, v| v[0] * v[1] if v.length == 2 }.sum
 end
 
-p part_1 input
+solve input
